ba95699720852daf4a94010530281bdec145b53f
Jeronimo Rivera <jeronimoriveramonroy@gmail.com>
2026-05-23 12:05:12 -0500

    INFO JERO

---
+++ devportfolio-master/src/config.ts
@@ -0,0 +1,87 @@
+export const siteConfig = {
+  name: "Jeronimo Rivera",
+  title: "Software Developer",
+  description: "Portfolio website of JERO",
+  accentColor: "#23c7dd",
+  social: {
+    email: "jeronimoriveramonroy@gmail.com",
+    linkedin: "www.linkedin.com/in/jero05",
+    github: "https://github.com/jerorivera",
+  },
+  aboutMe:
+    "I am a Software Development Technologist, graduated from Universidad del Valle – Zarzal Campus, with a strong passion for programming and web application development focused on efficiency and user experience. I have knowledge of programming languages such as Java, Python, and PHP, as well as experience with web technologies HTML, CSS, and JavaScript.I stand out for my ability to solve complex problems, optimize processes, and build functional, well-structured solutions. During my academic training, I worked on several practical projects, including the development of a website for a local restaurant and an institutional web platform for Universidad del Valle – Zarzal Campus, allowing me to apply my skills in real-world scenarios.These experiences helped me strengthen my teamwork, communication, and collaborative development skills, while maintaining a mindset of continuous learning. I am currently motivated to grow professionally and contribute to software development and web application projects.",
+  skills: ["Javascript", "React", "Node.js", "Python", "AWS", "SQL"],
+  projects: [
+    {
+      name: "Marketplace Univalle",
+      description:
+        "Es una pagina web que permite a estudiantes con correo institucional de la Universidad del Valle comprar y vender artículos de forma segura, fomentando un entorno colaborativo e integrado dentro de la comunidad universitaria",
+      link: "https://marketplace.intep.edu.co/",
+      skills: ["TypeScript", "PHP", "CSS", "Laravel"],
+    },
+    {
+      name: "Desarrollo web local Anttony Pizza",
+      description:
+        "Diseño e implementación de una estructura web local para la gestión contable del restaurante, contribuyendo a la optimización de procesos, el análisis de información y la mejora continua de la empresa.",
+      skills: ["Javascript", "Python", "CSS", "Laravel", "PHP"],
+    },
+    {
+      name: "Nestle de Colombia",
+      description:
+        "Implementación de dashboards estandarizados en Power BI (KSR) conectados a SQL, garantizando automatización, consistencia y disponibilidad de datos a nivel regional y Participaciones en proyectos de transformación digital con fábricas de Nestlé a nivel Latinoamérica y Estados Unidos.",
+      skills: ["Power BI", "SQL", "PowerApps", "SAP"],
+    },
+  ],
+  experience: [
+    {
+      company: "Nestle de Colombia",
+      title: "Analista I&R",
+      dateRange: "2026 - Present",
+      bullets: [
+        "Data analysis",
+        "Automate manual reports",
+        "Identify trends and opportunities for improvement",
+        "Analyze product or market performance",
+      ],
+    },
+    {
+      company: "Nestle de Colombia",
+      title: "Practicante Universitario Manufacturing Excellence - TPM",
+      dateRange: "2025 - 2026",
+      bullets: [
+        "Participation in digital transformation projects with Nestlé factories across Latin America",
+        "Development of digital solutions for the analysis of production downtime, contributing to data-driven decision-making and continuous improvement",
+        "Contribution to a 6% reduction in unplanned downtime at the Nestlé Bugalagrande plant, through the application of best practices in development, data analysis and continuous improvement",
+      ],
+    },
+    {
+      company: "Pizzeria Anttony Pizza",
+      title: "Desarrollador Web",
+      dateRange: "2024 - 2025",
+      bullets: [
+        "Collaboration in the design and implementation of data structures oriented towards information analysis and improvement of operational processes",
+        "Support in optimizing the user experience and in integrating data for subsequent analysis",
+      ],
+    },
+  ],
+  education: [
+    {
+      school: "Universidad Del Valle",
+      degree: "Tecnologo en Desarrollo de Software",
+      dateRange: "2022 - 2026",
+      achievements: [
+        "Reconocimientos académicos desde el tercer semestre del pregrado en Tecnología en Desarrollo de Software, por ocupar los 5 primeros puestos del programa de manera consecutiva",
+        "Desarrollo de la página web “Marketplace Univalle”, una plataforma digital que permite a estudiantes con correo institucional de la Universidad del Valle comprar y vender artículos de forma segura, fomentando un entorno colaborativo e integrado dentro de la comunidad universitaria",
+      ],
+    },
+    {
+      school: "Liceo San Antonio",
+      degree: "Bachiller Técnico Industrial",
+      dateRange: "2021",
+      achievements: [
+        "Reconocimiento por mejor Proyecto de Grado",
+        "Recnocimientos por excelencia Academica",
+      ],
+    },
+  ],
+};
