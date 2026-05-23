145cefd833f7e92123ad349bab590997945b4cd2
Jeronimo Rivera <jeronimoriveramonroy@gmail.com>
2026-05-23 12:23:14 -0500

    Fix Astro GitHub Pages

--- devportfolio-master/astro.config.mjs
+++ devportfolio-master/astro.config.mjs
@@ -4,6 +4,9 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://jerorivera.github.io",
+  base: "/jerorivera.info",
+
   vite: {
     plugins: [tailwindcss()],
   },
